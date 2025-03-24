66ca102fe173ff2ed414668be90e6b2cd0d4e3cb
Jeffrey <layolaj@yahoo.com>
2025-03-24 16:45:14 -0700

    another push

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-let dollars = [1, 5, 10, 3];
+let users = [
+    {
+        username: "Jeffrey", 
+        email: "jlayola@yahoo.com",
+        password: "test123",
+        subscriptionStatus: "VIP",
+        discordID: "Jeffrey Layola#0001",
+        lessonsCompleted: [0, 1],
+    },
+    {
+        username: "David", 
+        email: "david@yahoo.com",
+        password: "david123",
+        subscriptionStatus: "VIP",
+        discordID: "Jeffrey Layola#0001",
+        lessonsCompleted: [0, 1, 2, 3],
+    },
+];
 
-let cents = [];
+function login(email, password) {
+    for (let i = 0; i < users.length; ++i) {
+    if (users[i].email === email) {
+        console.log(users[i]);
+        if (users[i].password === password) {
+            console.log ('log the user in - the details are correct')
+        }
+        else{
+            console.log ('password is incorrect')
+        }
+      }  
+    }
+}
 
-for (let i =0; i <dollars.length; ++i)
-    cents.push(dollars [i] * 100);
+login ('jlayola@yahoo.com', 'wrongpassword')
 
-console.log(cents)
+
+
+function register (user) {
+   users.push(user);
+  
+}
+
+register ({username: "David", email: 'david@yahoo.com', password: 'david123', subscriptionStatus: 'VIP', discordID: 'Jeffrey Layola#0002', lessonsCompleted: [0, 1]});
+
+console.log (users);
